f5a119d6bb790b87dc419df9b938c90bb7e68961
Keroles-Nadyy <keroles_N@outlook.com>
2024-03-09 10:38:00 +0200

    modify database_validate function in utils_functions.sh script to be more general

--- utils_functions.sh
+++ utils_functions.sh
@@ -1,34 +1,35 @@
 
 database_validate(){
-    db_name=$1
+    db_object_name=${1:-$db_name}
+    db_object=${2:-Database}
     
     if [[ $db_name =~ [[:space:]] ]]
     then   
-        echo -e "${RED_Highlight_bold}Database name cannot contain space.${RESET}"
+        echo -e "${RED_Highlight_bold}${db_object} name cannot contain space.${RESET}"
         return 1
     fi
 
     if [ -z $db_name ]
     then
-        echo -e "${RED_Highlight_bold}Database name cannot be empty.${RESET}"
+        echo -e "${RED_Highlight_bold}${db_object} name cannot be empty.${RESET}"
         return 1
     fi
 
-    if [ ${#db_name} -lt 3 ]
+    if [ ${#db_name} -lt 2 ]
     then
-        echo -e "${RED_Highlight_bold}Database name must be at least 3 characters long.${RESET}"
+        echo -e "${RED_Highlight_bold}${db_object} name must be at least 3 characters long.${RESET}"
         return 1
     fi
 
     if [[ ! $db_name =~ ^[a-zA-Z] ]]
     then
-        echo -e "${RED_Highlight_bold}Database name must start with a letter.${RESET}"
+        echo -e "${RED_Highlight_bold}${db_object} name must start with a letter.${RESET}"
         return 1
     fi
 
     if [[ ! $db_name =~ ^[a-zA-Z0-9_]+$ ]] 
     then
-        echo -e "${RED_Highlight_bold}Database name can only contain alphanumeric characters and underscores.${RESET}"
+        echo -e "${RED_Highlight_bold}${db_object} name can only contain alphanumeric characters and underscores.${RESET}"
         return 1
     fi
 }
