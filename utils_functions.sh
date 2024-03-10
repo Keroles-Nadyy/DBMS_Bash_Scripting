d77e1e6809f73789c16edae296c771d077b3a283
Keroles-Nadyy <keroles_N@outlook.com>
2024-03-10 12:50:41 +0200

    modify database_validate function

--- utils_functions.sh
+++ utils_functions.sh
@@ -3,31 +3,31 @@ database_validate(){
     db_object_name=${1:-$db_name}
     db_object=${2:-Database}
     
-    if [[ $db_name =~ [[:space:]] ]]
+    if [[ $db_object_name =~ [[:space:]] ]]
     then   
         echo -e "${RED_Highlight_bold}${db_object} name cannot contain space.${RESET}"
         return 1
     fi
 
-    if [ -z $db_name ]
+    if [ -z $db_object_name ]
     then
         echo -e "${RED_Highlight_bold}${db_object} name cannot be empty.${RESET}"
         return 1
     fi
 
-    if [ ${#db_name} -lt 2 ]
+    if [ ${#db_object_name} -lt 2 ]
     then
         echo -e "${RED_Highlight_bold}${db_object} name must be at least 3 characters long.${RESET}"
         return 1
     fi
 
-    if [[ ! $db_name =~ ^[a-zA-Z] ]]
+    if [[ ! $db_object_name =~ ^[a-zA-Z] ]]
     then
         echo -e "${RED_Highlight_bold}${db_object} name must start with a letter.${RESET}"
         return 1
     fi
 
-    if [[ ! $db_name =~ ^[a-zA-Z0-9_]+$ ]] 
+    if [[ ! $db_object_name =~ ^[a-zA-Z0-9_]+$ ]] 
     then
         echo -e "${RED_Highlight_bold}${db_object} name can only contain alphanumeric characters and underscores.${RESET}"
         return 1
